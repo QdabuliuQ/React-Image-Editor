61fb4c2da3c921367a5e252c903899d4f065a931
zhengjihao <zhengjihao@kkgroup.cn>
2024-06-21 16:30:13 +0800

    fix: CI/CD 自动化部署

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/react-image-editor/",
   plugins: [react()],
   resolve: {
     alias: {
