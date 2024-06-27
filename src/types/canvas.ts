8ad5cbad5673e884d3621e2914d29eb5647bdd5c
QdabuliuQ <2714499297@qq.com>
2024-06-27 22:56:11 +0800

    fix: 修复ts语法错误

--- src/types/canvas.ts
+++ src/types/canvas.ts
@@ -2,8 +2,8 @@ export interface Canvas {
   width: number;
   height: number;
   fill: string;
-  loadFromJSON: (json: any) => any;
-  toDatalessJSON: (...args: Array<any>) => any;
-  forEachObject: (callback: (object: any) => void) => void;
-  remove: (object: any) => void;
+  loadFromJSON?: (json: any) => any;
+  toDatalessJSON?: (...args: Array<any>) => any;
+  forEachObject?: (callback: (object: any) => void) => void;
+  remove?: (object: any) => void;
 }
