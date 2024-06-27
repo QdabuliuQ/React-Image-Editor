550c9c83ab7fa49dc2b2edebec8b9505244b7b66
QdabuliuQ <2714499297@qq.com>
2024-06-27 23:38:47 +0800

    fix: 修改vite.config配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-image-editor/",
+  base: "/React-Image-Editor/",
   plugins: [react()],
   resolve: {
     alias: {
